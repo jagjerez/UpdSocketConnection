60b5f61c23319e9fbb8d75962544a61a009f47ef
jose alejandro godoy <desarrollo1@estacionmaritima.com>
2021-07-09 18:49:16 +0200

    Add project files.

---
+++ Applicacion1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Applicacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
